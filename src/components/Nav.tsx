9d98df48ef4fee6b2d2416d29cc501b52d83d8ab
coreybriscoe <coreybriscoe@gmail.com>
2023-03-18 23:02:32 -0400

    Create branch dev

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -48,7 +48,6 @@ const getNavItems = (): JSX.Element => {
               <Link to={page.path} className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">{page.name}</Link>
             </li>
           )
-
         );
       })}
     </>
@@ -72,7 +71,6 @@ export default function Nav() {
           </ul>
         </div>
       </div>
-
     </nav>
   );
 }
