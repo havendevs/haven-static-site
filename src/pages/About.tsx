f12a7880c7318a3b896aece406f766dab2f015a6
coreybriscoe <coreybriscoe@gmail.com>
2023-03-20 02:37:49 -0400

    Add About page text without styling or images

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,8 +2,74 @@ import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="pt-[7rem] h-full">
+      <div className="mx-5 md:ml-20 md:w-auto">
+        <h1>About Us</h1>
+        <h3 className="text-2xl ml-3 mt-4 text-center">
+          Founded in Bloomington, Indiana, Haven Transportation LLC aims to legitimize and innovate
+          micromobility by providing municipalities with our proprietary software platform, ebikes,
+          escooters, and docks -- Havens™ -- to improve the quality of and control over this
+          quickly growing form of transportation. Our
+          products and platform address an unmet need in the American market for a flexible and
+          modular docked-based micromobility vendor that municipalities can use to improve safety,
+          accessibility to transit, and environmental consciousness without exorbitant expense. We
+          intend to partner with cities and start pilot programs that will mark a shift in the
+          transportation systems of America.
+        </h3>
+      </div>
+      <div className="bg-slate-800 text-gray-50">
+        <div>
+          <h2>Our Team</h2>
+          <div>
+            <div>
+              <h4>Zade Dohan</h4>
+              <p>CEO and Founder</p>
+              <p>
+                BS Computer Science, WGU --&gt;MBA Business Analytics, Kelley School of Business
+              </p>
+            </div>
+            <div>
+              <h4>Corey Briscoe</h4>
+              <p>CTO and Founder</p>
+              <p>BS/MS Computer Science, Luddy School of Informatics, Computing, and Engineering</p>
+            </div>
+            <div>
+              <h4>Lucas Kroll</h4>
+              <p>Marketing Advisor</p>
+              <p>BS Marketing, Kelley School of Business</p>
+            </div>
+            <div>
+              <h4>Will Pack</h4>
+              <p>Financial Strategy Advisor</p>
+              <p>BS Supply Chain Management, Kelley School of Business</p>
+            </div>
+            <div>
+              <h4>Samuel Estrada</h4>
+              <p>Software Development Advisor</p>
+              <p>BS/MS Computer Science, Luddy School of Informatics, Computing, and Engineering</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+        <div>
+          <p>
+            As students at Indiana University, we&aposve see the impact of micromobility on
+            communities first-hand and decided to found Haven to help improve public transportation
+            by promoting sustainability, safety, and reliability in ways that the current market
+            cannot.
+          </p>
+          <p>
+            Our team&apos;s early steps in the commuter transportation industry began in July 2021,
+            when we conducted market research, consulted iteratively with institutions like IU
+            Parking Operations, and developed IP to modernize parking infrastructure. What would go
+            on to become Haven officially kicked off in October 2022. We have been working to
+            legitimize and improve publicly-operated micro-mobility solutions ever since.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
